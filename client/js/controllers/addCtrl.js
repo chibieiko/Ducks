3913efffebd928359b0080bf9feef918990bb80b
Erika Sankari <8eikochan@gmail.com>
2017-01-20 21:41:19 +0200

    Add standard date filter interceptor to enforce timezone +0000.

--- client/js/controllers/addCtrl.js
+++ client/js/controllers/addCtrl.js
@@ -26,15 +26,18 @@
 
             $scope.saveSighting = function () {
                 $scope.sighting.species = $scope.selectedSpecies.name;
-                var tempDate = moment($scope.sighting.date);
-                console.log(tempDate.utc());
+                var tempDate = moment($scope.sighting.dateTime, "DD/MM/YYYY HH:mm");
+                console.log(tempDate.format);
+                $scope.sighting.dateTime = tempDate.toISOString();
 
                 console.log($scope.sighting);
+                console.log($scope.sighting.date);
 
                 DuckService.addSighting($scope.sighting, function (res, err) {
                     if (!err) {
                         console.log("Successfully saved");
                         $scope.sighting = {};
+                        $scope.selectedSpecies = {};
                     } else {
                         console.log("Failed to save")
                     }
