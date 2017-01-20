25280d92121a4f1175c9ec90cfd6853fe3109d81
Erika Sankari <8eikochan@gmail.com>
2017-01-20 22:54:28 +0200

    Validate fields.

--- client/js/controllers/addCtrl.js
+++ client/js/controllers/addCtrl.js
@@ -2,13 +2,11 @@
     "use strict";
 
     angular.module("duckModule").controller("AddCtrl",
-        function ($scope, DuckService) {
+        function ($scope, DuckService, $location) {
 
             $scope.sighting = {
                 count: 1
             };
-
-            $scope.selectedSpecies = {};
 
             function getSpecies() {
                 DuckService.getSpecies(function (res, err) {
@@ -26,23 +24,23 @@
 
             $scope.saveSighting = function () {
                 $scope.sighting.species = $scope.selectedSpecies.name;
-                var tempDate = moment($scope.sighting.dateTime, "DD/MM/YYYY HH:mm");
-                console.log(tempDate.format);
+                var tempDate = moment($scope.dateTime, "DD/MM/YYYY HH:mm");
                 $scope.sighting.dateTime = tempDate.toISOString();
 
                 console.log($scope.sighting);
-                console.log($scope.sighting.date);
 
-                DuckService.addSighting($scope.sighting, function (res, err) {
-                    if (!err) {
-                        console.log("Successfully saved");
-                        $scope.sighting = {};
-                        $scope.selectedSpecies = {};
-                    } else {
-                        console.log("Failed to save")
-                    }
-                })
+                if ($scope.sighting.description !== undefined) {
+                    DuckService.addSighting($scope.sighting, function (res, err) {
+                        if (!err) {
+                            console.log("Successfully saved");
+                            $location.path("/");
+                        } else {
+                            console.log("Failed to save")
+                        }
+                    })
+                } else {
+                    console.log("Please provide a valid description");
+                }
             };
-
         });
 }());
